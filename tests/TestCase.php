4a8c0709c70884d02e2657b952dc39163e5c5c5b
Tim MacDonald <hello@timacdonald.me>
2022-11-25 11:20:44 +1100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,31 +9,5 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends OrchestraTestCase
 {
-    protected function defineEnvironment($app)
-    {
-        Artisan::call('vendor:publish', ['--tag' => 'laravel-package-assets']);
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // LaravelPackage::$authUsing = function () {
-        //     return true;
-        // };
-    }
-
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-
-        // LaravelPackage::$authUsing = null;
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [];
-
-        return [LaravelPackageServiceProvider::class];
-    }
+    //
 }
