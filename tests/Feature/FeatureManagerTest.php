e7e98b0cbf6cdbe9d32bb302513de4ea163246f0
Tim MacDonald <hello@timacdonald.me>
2022-11-25 11:18:29 +1100

    wip

---
+++ tests/Feature/FeatureManagerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class FeatureManagerTest extends TestCase
+{
+    public function test_it_works()
+    {
+        $this->assertTrue(true);
+    }
+}
