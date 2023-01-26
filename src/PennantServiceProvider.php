9312438c0ca7fc22fbed447e131bb6416c9e9717
Taylor Otwell <taylor@laravel.com>
2023-01-26 15:54:17 +0000

    add blade directive

--- src/PennantServiceProvider.php
+++ src/PennantServiceProvider.php
@@ -27,21 +27,30 @@ class PennantServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-            $this->publishes([
-                __DIR__.'/../config/pennant.php' => config_path('pennant.php'),
-            ], 'pennant-config');
-
-            $this->publishes([
-                __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
-            ], 'laravel-pennant-migrations');
+            $this->offerPublishing();
 
             $this->commands([
                 \Laravel\Pennant\Commands\PurgeCommand::class,
             ]);
         }
 
+        $this->app->afterResolving('blade', function ($blade) {
+            $blade->if('feature', function ($value) {
+                return Feature::active($value);
+            });
+        });
+
+
+        $this->listenForEvents();
+    }
+
+    /**
+     * Listen for the events relevant to the package.
+     *
+     * @return void
+     */
+    protected function listenForEvents()
+    {
         $this->app['events']->listen([
             \Laravel\Octane\Events\RequestReceived::class,
             \Laravel\Octane\Events\TaskReceived::class,
@@ -53,5 +62,23 @@ class PennantServiceProvider extends ServiceProvider
         $this->app['events']->listen([
             \Illuminate\Queue\Events\JobProcessed::class,
         ], fn () => $this->app[FeatureManager::class]->flushCache());
+    }
+
+    /**
+     * Register the migrations and publishing for the package.
+     *
+     * @return void
+     */
+    protected function offerPublishing()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../config/pennant.php' => config_path('pennant.php'),
+        ], 'pennant-config');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
+        ], 'laravel-pennant-migrations');
     }
 }
