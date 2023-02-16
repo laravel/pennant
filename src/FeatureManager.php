a482e164eb89eb4439b2fdcb9c7aa5542cdca2cb
Wong Ban Korh <wongbankorh@gmail.com>
2023-02-17 00:59:20 +0800

    Fix table name not being used
    The pennant config is using `stores` and same when creating driver in this file, however, when configuring database driver, it reads from `drivers` which is not found.

--- src/FeatureManager.php
+++ src/FeatureManager.php
@@ -77,7 +77,7 @@ class FeatureManager extends Manager
      */
     public function createDatabaseDriver()
     {
-        return with($this->container['config']->get('pennant.drivers.database'), function ($config) {
+        return with($this->container['config']->get('pennant.stores.database'), function ($config) {
             return new DatabaseDriver(
                 $this->container['db']->connection($config['connection'] ?? null),
                 $this->container['events'],
