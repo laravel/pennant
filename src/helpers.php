a7c57d7cdabbac87e838d8d99d4c5cc8bc7ce806
Randy <chenrw@topode.com>
2023-02-22 11:32:26 +0800

    pref: create if function does not exist

--- src/helpers.php
+++ src/helpers.php
@@ -3,21 +3,23 @@
 use Illuminate\Container\Container;
 use Laravel\Pennant\FeatureManager;
 
-/**
- * Get the feature manager instance.
- *
- * @param  string|null  $name
- * @param  \Closure|null  $whenActive
- * @param  \Closure|null  $whenInactive
- * @return \Laravel\Pennant\FeatureManager|mixed
- */
-function feature($name = null, $whenActive = null, $whenInactive = null)
-{
-    $manager = Container::getInstance()->make(FeatureManager::class);
+if (! function_exists('feature')) {
+    /**
+     * Get the feature manager instance.
+     *
+     * @param string|null $name
+     * @param \Closure|null $whenActive
+     * @param \Closure|null $whenInactive
+     * @return \Laravel\Pennant\FeatureManager|mixed
+     */
+    function feature($name = null, $whenActive = null, $whenInactive = null)
+    {
+        $manager = Container::getInstance()->make(FeatureManager::class);
 
-    return match (func_num_args()) {
-        0 => $manager,
-        1 => $manager->value(...func_get_args()),
-        default => $manager->when(...func_get_args()),
-    };
+        return match (func_num_args()) {
+            0 => $manager,
+            1 => $manager->value(...func_get_args()),
+            default => $manager->when(...func_get_args()),
+        };
+    }
 }
