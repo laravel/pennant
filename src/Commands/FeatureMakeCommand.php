c5df3ae2a8371b05db2fdcf7870ea2381e302eb8
Nuno Maduro <enunomaduro@gmail.com>
2023-02-23 12:34:57 +0000

    [1.x] Adds type checking (#47)
    * Adds type checking
    
    * Fix code styling
    
    ---------
    
    Co-authored-by: nunomaduro <nunomaduro@users.noreply.github.com>

--- src/Commands/FeatureMakeCommand.php
+++ src/Commands/FeatureMakeCommand.php
@@ -3,6 +3,7 @@
 namespace Laravel\Pennant\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputOption;
 
 #[AsCommand(name: 'pennant:feature')]
