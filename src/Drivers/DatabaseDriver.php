401e9051a3a600296c81c3733b52949c60750f02
Tim MacDonald <hello@timacdonald.me>
2024-03-05 09:18:21 +1100

    Ensure strings (#91)

--- src/Drivers/DatabaseDriver.php
+++ src/Drivers/DatabaseDriver.php
@@ -61,14 +61,14 @@ class DatabaseDriver implements CanListStoredFeatures, Driver
     /**
      * The name of the "created at" column.
      *
-     * @var string|null
+     * @var string
      */
     const CREATED_AT = 'created_at';
 
     /**
      * The name of the "updated at" column.
      *
-     * @var string|null
+     * @var string
      */
     const UPDATED_AT = 'updated_at';
 
