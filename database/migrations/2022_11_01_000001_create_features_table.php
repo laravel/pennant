a664ac8873c54a382981826bb0612019652de8f4
Davendra Naraine <davendra.naraine@nived.io>
2025-03-21 21:22:27 -0400

    add return types for migrations (#137)

--- database/migrations/2022_11_01_000001_create_features_table.php
+++ database/migrations/2022_11_01_000001_create_features_table.php
@@ -11,7 +11,7 @@ return new class extends PennantMigration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('features', function (Blueprint $table) {
             $table->id();
@@ -29,7 +29,7 @@ return new class extends PennantMigration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('features');
     }
