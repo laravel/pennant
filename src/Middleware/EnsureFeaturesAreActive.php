278b25746c767e595d5f0a26f096c311b5b4bce2
Simon Hamp <simon.hamp@me.com>
2025-05-30 17:43:11 +0100

    Check environment

--- src/Middleware/EnsureFeaturesAreActive.php
+++ src/Middleware/EnsureFeaturesAreActive.php
@@ -18,9 +18,13 @@ class EnsureFeaturesAreActive
         Feature::loadMissing($features);
 
         if (Feature::someAreInactive($features)) {
+            $error = config('app.debug')
+                ? 'Required features ['.join(', ', $features).'] not enabled.'
+                : 'Required features not enabled.';
+
             return static::$respondUsing
                 ? call_user_func(static::$respondUsing, $request, $features)
-                : abort(400, 'Features ['.join(', ', $features).'] not enabled.');
+                : abort(400, $error);
         }
 
         return $next($request);
