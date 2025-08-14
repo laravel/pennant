b0ef3346acdf63dee2d8110f19ad1b24fb0f0f8a
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2025-08-14 17:32:06 +0000

    Update facade docblocks

--- src/Feature.php
+++ src/Feature.php
@@ -29,7 +29,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static void purge(string|array|null $features = null)
  * @method static string name(string $feature)
  * @method static array nameMap()
- * @method static mixed instance(string $name)
+ * @method static void instance(void $name)
  * @method static \Laravel\Pennant\Contracts\Driver getDriver()
  * @method static void macro(string $name, object|callable $macro)
  * @method static void mixin(object $mixin, bool $replace = true)
