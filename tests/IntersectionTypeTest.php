c20a05b196d1ecd28497e71fafb93fc12e84840c
Mior Muhammad Zaki <crynobone@gmail.com>
2025-11-20 19:02:37 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/IntersectionTypeTest.php
+++ tests/IntersectionTypeTest.php
@@ -5,6 +5,7 @@ namespace Tests;
 use Illuminate\Contracts\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Laravel\Pennant\Feature;
 use PHPUnit\Framework\Attributes\RequiresPhp;
